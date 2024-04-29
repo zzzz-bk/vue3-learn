7604ef11713ffb49764c92e735602f446169adf9
zhengyb <zhengyb@allinpay.com>
2024-04-29 18:02:04 +0800

    global style and tools

--- shop-admin/src/App.vue
+++ shop-admin/src/App.vue
@@ -14,12 +14,31 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  background-color: #f1f1f1;
+}
+.content-container {
+  background-color: white;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 10px;
+  min-width: 1200px;
+  display: inline-block;
+  width: 90%;
+}
+.input-tip {
+  margin: 0 10px;
+  line-height: 40px;
+}
+.input-field {
+  margin-right: 40px;
+}
+.content-row {
+  margin-bottom: 20px;
 }
 </style>
